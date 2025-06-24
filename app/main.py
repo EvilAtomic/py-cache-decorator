c759fcf89810fd4b17cf0e6c7c79a6a5e401c848
artem <artemasarkisan598@gmail.com>
2025-06-24 17:11:00 +0300

    Impliment py-Cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_dict:
+                print("Getting from cache")
+                return cache_dict[key]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_dict[key] = result
+        return result
+    return wrapper
+
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> list[int]:
+    return [number ** power for number in n_tuple]
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
