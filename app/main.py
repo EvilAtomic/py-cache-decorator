23330f5092ff1b1b92343955125a70f9968c70a2
artem <artemasarkisan598@gmail.com>
2025-06-24 17:48:41 +0300

    Impliment py-Cache decorator

--- app/main.py
+++ app/main.py
@@ -15,19 +15,3 @@ def cache(func: Callable) -> Callable:
             cache_dict[key] = result
         return result
     return wrapper
-
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list[int]:
-    return [number ** power for number in n_tuple]
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
