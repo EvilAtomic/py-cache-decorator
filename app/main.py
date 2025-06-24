5d0f9251229556ced4446ed356fd356d196af8a9
artem <artemasarkisan598@gmail.com>
2025-06-24 18:32:17 +0300

    Impliment py-Cache decorator

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def long_time_func(a: int, b: int, c: int) -> int:
     return (a ** b ** c) % (a * c)
 
 @cache
-def long_time_func_2(n_tuple: tuple, power: int) -> list[int]:
+def long_time_func_2(n_tuple: tuple, power: int) -> list:
     return [number ** power for number in n_tuple]
 
 long_time_func(1, 2, 3)
